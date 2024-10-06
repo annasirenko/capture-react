ca3264c305e83af5ffabf9bad6a17edaef8cd1f7
Anna <otherjam@gmail.com>
2024-10-06 19:41:21 +0300

    add finished version of code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
+//Import Pages
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
+//Router
+import { Routes, Route } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
+
 function App() {
+  window.onload = function () {
+    setTimeout(function () {
+      window.scrollTo(0, 0);
+    }, 1);
+  };
   return (
     <div className="App">
+      <GlobalStyle />
+      <Nav />
+      <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
+        <Routes>
+          <Route path="/" element={<AboutUs />} />
+          <Route path="/work" element={<OurWork />} />
+          <Route path="/work/:id" element={<MovieDetail />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,28 @@
+import React from "react";
+//Page Components
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+import ScrollTop from "../components/ScrollTop";
+import { Hide } from "../styles";
+
+const AboutUs = () => {
+  return (
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
+      <AboutSection />
+      <Hide><ServicesSection /></Hide>
+      <Hide><FaqSection /></Hide>
+      <ScrollTop />
+    </motion.div>
+  );
+};
+
+export default AboutUs;
